3449525e33add5b5520e05757b775daff3ab8804
Décio Luiz Gazzoni Filho <decio@decpp.net>
2018-10-30 23:12:50 -0300

    Correção da numeração dos exercícios.

--- arvbin.c
+++ arvbin.c
@@ -4,73 +4,73 @@
 
 void arvbin_inicializar(struct arvbin* arv, int(*compar)(void*, void*))
 {
-	// Insira aqui o codigo da sua solucao do exercicio 16
+	// Insira aqui o codigo da sua solucao do exercicio 1
 }
 
 void arvbin_inserir(struct arvbin* arv, void* dado)
 {
-	// Insira aqui o codigo da sua solucao do exercicio 17
+	// Insira aqui o codigo da sua solucao do exercicio 2
 }
 
 struct arvbin_no* arvbin_minimo(struct arvbin* arv)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 18
+	// Substitua o codigo abaixo pela sua solucao do exercicio 3
 	return NULL;
 }
 
 struct arvbin_no* arvbin_maximo(struct arvbin* arv)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 19
+	// Substitua o codigo abaixo pela sua solucao do exercicio 4
 	return NULL;
 }
 
 struct arvbin_no* arvbin_predecessor(struct arvbin* arv, struct arvbin_no* no)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 20
+	// Substitua o codigo abaixo pela sua solucao do exercicio 5
 	return NULL;
 }
 
 struct arvbin_no* arvbin_sucessor(struct arvbin* arv, struct arvbin_no* no)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 21
+	// Substitua o codigo abaixo pela sua solucao do exercicio 6
 	return NULL;
 }
 
 void arvbin_remover(struct arvbin* arv, struct arvbin_no* no)
 {
-	// Insira aqui o codigo da sua solucao do exercicio 22
+	// Insira aqui o codigo da sua solucao do exercicio 7
 }
 
 struct arvbin_no* arvbin_buscar(struct arvbin* arv, void* dado)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 23
+	// Substitua o codigo abaixo pela sua solucao do exercicio 8
 	return NULL;
 }
 
 void arvbin_visitar_em_ordem(struct arvbin* arv, void(*func_visita)(struct arvbin_no*))
 {
-	// Insira aqui o codigo da sua solucao do exercicio 24
+	// Insira aqui o codigo da sua solucao do exercicio 9
 }
 
 void arvbin_visitar_em_ordem_inversa(struct arvbin* arv, void(*func_visita)(struct arvbin_no*))
 {
-	// Insira aqui o codigo da sua solucao do exercicio 25
+	// Insira aqui o codigo da sua solucao do exercicio 10
 }
 
 int arvbin_contar_elementos(struct arvbin* arv)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 26
+	// Substitua o codigo abaixo pela sua solucao do exercicio 11
 	return -1;
 }
 
 int arvbin_altura_elemento(struct arvbin* arv, struct arvbin_no* no)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 27
+	// Substitua o codigo abaixo pela sua solucao do exercicio 12
 	return -1;
 }
 
 int arvbin_profundidade(struct arvbin* arv)
 {
-	// Substitua o codigo abaixo pela sua solucao do exercicio 28
+	// Substitua o codigo abaixo pela sua solucao do exercicio 13
 	return -1;
 }
